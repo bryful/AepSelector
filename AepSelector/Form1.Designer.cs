ada47de3a1d0d71f1f36e0697677985c87c6a5ff
bryful <bryful@gmail.com>
2019-07-08 13:50:01 +0900

    プロジェクト ファイルを追加します。

---
+++ AepSelector/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace AepSelector
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.edAfx = new System.Windows.Forms.ListBox();
+			this.edAep = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// edAfx
+			// 
+			this.edAfx.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.edAfx.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.edAfx.FormattingEnabled = true;
+			this.edAfx.ItemHeight = 16;
+			this.edAfx.Location = new System.Drawing.Point(12, 41);
+			this.edAfx.Name = "edAfx";
+			this.edAfx.Size = new System.Drawing.Size(494, 132);
+			this.edAfx.TabIndex = 2;
+			// 
+			// edAep
+			// 
+			this.edAep.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.edAep.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.edAep.Location = new System.Drawing.Point(12, 12);
+			this.edAep.Name = "edAep";
+			this.edAep.ReadOnly = true;
+			this.edAep.Size = new System.Drawing.Size(494, 23);
+			this.edAep.TabIndex = 3;
+			// 
+			// Form1
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(514, 182);
+			this.Controls.Add(this.edAep);
+			this.Controls.Add(this.edAfx);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+			this.Name = "Form1";
+			this.Text = "AepSelector";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+			this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.ListBox edAfx;
+		private System.Windows.Forms.TextBox edAep;
+	}
+}
+
